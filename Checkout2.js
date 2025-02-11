0df5c78bc13b42d9aee5e363d2d4403c165809a0
ckunkel510 <ckunkel@woodsonlumber.com>
2025-02-11 09:48:06 -0600

    Update Checkout2.js

--- Checkout2.js
+++ Checkout2.js
@@ -1,9 +1,11 @@
 // When the transaction type and other checkout divs are loaded
 $(document).ready(function() {
-    // Hide the original transaction type input content and all children of the transaction type div except for the label
+    console.log('Page loaded, initializing custom checkout experience...');
+
+    // Hide the original transaction type input content
     $('#ctl00_PageBody_TransactionTypeInput, .TransactionTypeSelector').hide();
 
-    // Create modern buttons to replace the transaction type radio buttons visually
+    // Create and append modern transaction type selector
     const modernTransactionSelector = `
         <div class="modern-transaction-selector d-flex justify-content-around">
             <button id="btnOrder" class="btn btn-primary" data-value="rdbOrder">
@@ -14,12 +16,11 @@ $(document).ready(function() {
             </button>
         </div>
     `;
-
-    // Append the modern selector after the transaction type label
     $('#ctl00_PageBody_TransactionTypeDiv').append(modernTransactionSelector);
 
-    // Handle transaction button clicks
+    // Transaction type button event handling
     function updateTransactionStyles(selectedValue) {
+        console.log(`Transaction type updated: ${selectedValue}`);
         if (selectedValue === 'rdbOrder') {
             $('#ctl00_PageBody_TransactionTypeSelector_rdbOrder').prop('checked', true);
             $('#btnOrder').removeClass('btn-secondary').addClass('btn-primary');
@@ -31,7 +32,7 @@ $(document).ready(function() {
         }
     }
 
-    // Initial update for transaction type buttons
+    // Initial transaction type update
     if ($('#ctl00_PageBody_TransactionTypeSelector_rdbOrder').is(':checked')) {
         updateTransactionStyles('rdbOrder');
     } else if ($('#ctl00_PageBody_TransactionTypeSelector_rdbQuote').is(':checked')) {
@@ -44,61 +45,68 @@ $(document).ready(function() {
     });
 
     // Hide original shipping method input and add modern buttons
-    $('.SaleTypeSelector').hide();
+    if ($('.SaleTypeSelector').length) {
+        $('.SaleTypeSelector').hide();
+        const modernShippingSelector = `
+            <div class="modern-shipping-selector d-flex justify-content-around">
+                <button id="btnDelivered" class="btn btn-primary" data-value="rbDelivered">
+                    <i class="fas fa-truck"></i> Delivered
+                </button>
+                <button id="btnPickup" class="btn btn-secondary" data-value="rbCollectLater">
+                    <i class="fas fa-store"></i> Pickup (Free)
+                </button>
+            </div>
+        `;
+        $('.epi-form-col-single-checkout:has(.SaleTypeSelector)').append(modernShippingSelector);
 
-    const modernShippingSelector = `
-        <div class="modern-shipping-selector d-flex justify-content-around">
-            <button id="btnDelivered" class="btn btn-primary" data-value="rbDelivered">
-                <i class="fas fa-truck"></i> Delivered
-            </button>
-            <button id="btnPickup" class="btn btn-secondary" data-value="rbCollectLater">
-                <i class="fas fa-store"></i> Pickup (Free)
-            </button>
-        </div>
-    `;
-
-    $('.epi-form-col-single-checkout:has(.SaleTypeSelector)').append(modernShippingSelector);
-
-    function updateShippingStyles(selectedValue) {
-        if (selectedValue === 'rbDelivered') {
-            $('#ctl00_PageBody_SaleTypeSelector_rbDelivered').prop('checked', true);
-            $('#btnDelivered').removeClass('btn-secondary').addClass('btn-primary');
-            $('#btnPickup').removeClass('btn-primary').addClass('btn-secondary');
-        } else if (selectedValue === 'rbCollectLater') {
-            $('#ctl00_PageBody_SaleTypeSelector_rbCollectLater').prop('checked', true);
-            $('#btnPickup').removeClass('btn-secondary').addClass('btn-primary');
-            $('#btnDelivered').removeClass('btn-primary').addClass('btn-secondary');
+        function updateShippingStyles(selectedValue) {
+            console.log(`Shipping method updated: ${selectedValue}`);
+            if (selectedValue === 'rbDelivered') {
+                $('#ctl00_PageBody_SaleTypeSelector_rbDelivered').prop('checked', true);
+                $('#btnDelivered').removeClass('btn-secondary').addClass('btn-primary');
+                $('#btnPickup').removeClass('btn-primary').addClass('btn-secondary');
+            } else if (selectedValue === 'rbCollectLater') {
+                $('#ctl00_PageBody_SaleTypeSelector_rbCollectLater').prop('checked', true);
+                $('#btnPickup').removeClass('btn-secondary').addClass('btn-primary');
+                $('#btnDelivered').removeClass('btn-primary').addClass('btn-secondary');
+            }
         }
-    }
 
-    if ($('#ctl00_PageBody_SaleTypeSelector_rbDelivered').is(':checked')) {
-        updateShippingStyles('rbDelivered');
-    } else if ($('#ctl00_PageBody_SaleTypeSelector_rbCollectLater').is(':checked')) {
-        updateShippingStyles('rbCollectLater');
-    }
+        if ($('#ctl00_PageBody_SaleTypeSelector_rbDelivered').is(':checked')) {
+            updateShippingStyles('rbDelivered');
+        } else if ($('#ctl00_PageBody_SaleTypeSelector_rbCollectLater').is(':checked')) {
+            updateShippingStyles('rbCollectLater');
+        }
 
-    $('.modern-shipping-selector button').on('click', function() {
-        const selectedValue = $(this).data('value');
-        updateShippingStyles(selectedValue);
-    });
+        $('.modern-shipping-selector button').on('click', function() {
+            const selectedValue = $(this).data('value');
+            updateShippingStyles(selectedValue);
+        });
+    } else {
+        console.warn('Shipping method selector not found.');
+    }
 
     // Hide the original date input and enhance the interface
-    $('#ctl00_PageBody_dtRequired_DatePicker_wrapper').hide();
+    if ($('#ctl00_PageBody_dtRequired_DatePicker_wrapper').length) {
+        $('#ctl00_PageBody_dtRequired_DatePicker_wrapper').hide();
 
-    const modernDateSelector = `
-        <div class="modern-date-selector">
-            <label for="modernDateInput">Date Required:</label>
-            <input type="text" id="modernDateInput" class="form-control" placeholder="Select a date">
-        </div>
-    `;
+        const modernDateSelector = `
+            <div class="modern-date-selector">
+                <label for="modernDateInput">Date Required:</label>
+                <input type="text" id="modernDateInput" class="form-control" placeholder="Select a date">
+            </div>
+        `;
+        $('.epi-form-col-single-checkout:has(#ctl00_PageBody_dtRequired_DatePicker_wrapper)').append(modernDateSelector);
 
-    $('.epi-form-col-single-checkout:has(#ctl00_PageBody_dtRequired_DatePicker_wrapper)').append(modernDateSelector);
-
-    $('#modernDateInput').datepicker({
-        dateFormat: 'mm/dd/yy',
-        minDate: 0,
-        onSelect: function(dateText) {
-            $('#ctl00_PageBody_dtRequired_DatePicker_dateInput').val(dateText);
-        }
-    });
+        $('#modernDateInput').datepicker({
+            dateFormat: 'mm/dd/yy',
+            minDate: 0,
+            onSelect: function(dateText) {
+                console.log(`Date selected: ${dateText}`);
+                $('#ctl00_PageBody_dtRequired_DatePicker_dateInput').val(dateText);
+            }
+        });
+    } else {
+        console.warn('Date picker wrapper not found.');
+    }
 });
