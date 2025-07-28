236e8f07c38c869f544c12f8da342e6c164990ff
ckunkel510 <ckunkel@woodsonlumber.com>
2025-07-28 21:37:22 +0000

    updtae

--- Addtocart.js
+++ Addtocart.js
@@ -16,6 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
     console.warn("[CartModal] Add to Cart button NOT FOUND ❌");
   }
 
+  document.querySelectorAll("a[id$='AddProductButton']").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("[CartModal] Product card Add to Cart clicked 🎯");
+    sessionStorage.setItem("showAddToCartModal", "true");
+  });
+});
+
+
   // ✅ Step 2: Check if we should show modal
   const modalFlag = sessionStorage.getItem("showAddToCartModal");
   console.log("[CartModal] Modal flag is:", modalFlag);
