390b08c4c9040c1c0713791e0b8ce3511fcee7d2
ckunkel510 <ckunkel@woodsonlumber.com>
2025-08-07 15:46:28 +0000

    updates

--- ShoppingCartRow.js
+++ ShoppingCartRow.js
@@ -2,42 +2,35 @@ $(function(){
   $('.shopping-cart-details .shopping-cart-item').each(function(){
     var $item = $(this);
 
-    // 1) clean out any hidden update controls + validators
+    // 1) strip out legacy update rows & validator
     $item.find('[style*="display: table-row"]').remove();
-    $item.find('[id*="QuantityValidator"]').hide();
+    $item.find('[id*="QuantityValidator"]').remove();
 
-    // 2) pull out & detach the interactive bits
-    var $saveBtn = $item.find('button.sfl-button').detach();
-    var $delBtn  = $item.find('a[id*="_del_"]').first().detach();
+    // 2) grab & detach delete button
+    var $delBtn = $item.find('a[id*="_del_"]').first().detach();
+
+    // 3) pull image, link & code, desc, price, qty+refresh, total
+    var imgSrc = $item.find('img.ThumbnailImage').attr('src');
+    var $infoCol = $item.find('.row.pl-2.w-100 .col-12.col-sm-6').first();
+    var $link   = $infoCol.find('a:has(.portalGridLink)').first();
+    var code    = $link.find('.portalGridLink').text().trim();
+    var href    = $link.attr('href');
+    // DESCRIPTION is in the 3rd child DIV
+    var desc    = $infoCol.find('> div:nth-child(3) div').text().trim();
+
+    var priceText = $item
+      .find('.col-12.col-sm-9 .col-6')
+      .contents().filter((i,n)=>n.nodeType===3)
+      .text().trim();
+
     var $qtyWrap = $item.find('span.RadInput').parent().detach();
     var $refresh = $item.find('a.refresh-cart-line-total').detach();
 
-    // 3) clone & clean the delete icon
-    var $delClone = $delBtn.clone().empty()
-                      .append( $delBtn.find('i').clone() );
-
-    // 4) clone qty & refresh so we get fresh copy
-    var $qtyClone = $qtyWrap.clone();
-    var $refClone = $refresh.clone();
-    var $saveClone= $saveBtn.clone();
-
-    // 5) grab the rest of your data
-    var imgSrc    = $item.find('img.ThumbnailImage').attr('src');
-    var $link     = $item.find('a:has(.portalGridLink)').first();
-    var code      = $link.find('.portalGridLink').text().trim();
-    var href      = $link.attr('href');
-    var desc      = $item
-                      .find('.col-12.col-sm-6').first()
-                      .find('> div').eq(2).text().trim();
-    var priceText = $item
-                      .find('.col-12.col-sm-9 .col-6')
-                      .contents().filter((i,n)=>n.nodeType===3)
-                      .text().trim();
     var totalText = $item
-                      .find('.col-12.col-sm-3 .d-flex div').first()
-                      .text().trim();
+      .find('.col-12.col-sm-3 .d-flex div').first()
+      .text().trim();
 
-    // 6) build a compact card
+    // 4) build new compact card with SFL placeholder
     var $card = $(`
       <div class="card mb-2 cart-item-card">
         <div class="card-body p-2">
@@ -45,8 +38,8 @@ $(function(){
             <div class="flex-shrink-0">
               <img src="${imgSrc}" alt="${code}"
                    style="
-                     width:10vw; max-width:100px;
-                     max-height:10vh; height:auto;
+                     width:12vw; max-width:120px;
+                     max-height:12vh; height:auto;
                      object-fit:cover; border-radius:4px;
                    ">
             </div>
@@ -56,22 +49,25 @@ $(function(){
               <div class="d-flex flex-wrap align-items-center">
                 <div class="me-3"><span class="fw-bold">${priceText}</span></div>
                 <div class="me-3 d-flex align-items-center">
-                  ${$qtyClone.prop('outerHTML')}
-                  ${$refClone.prop('outerHTML')}
+                  ${$qtyWrap.prop('outerHTML')}
+                  ${$refresh.prop('outerHTML')}
                 </div>
                 <div class="me-3"><span class="fw-bold">${totalText}</span></div>
               </div>
             </div>
             <div class="flex-shrink-0 text-end">
-              ${$delClone.prop('outerHTML')}
-              ${$saveClone.prop('outerHTML')}
+              <span class="del-btn"></span>
+              <span class="sfl-placeholder"></span>
             </div>
           </div>
         </div>
       </div>
     `);
 
-    // 7) replace the old markup
+    // 5) inject delete button into card
+    $card.find('.del-btn').append($delBtn);
+
+    // 6) swap out the old markup
     $item.empty().append($card);
   });
 });
