276f34e1848b37b520f7395dd76ddc328a5231cb
ckunkel510 <ckunkel@woodsonlumber.com>
2025-08-07 21:08:24 +0000

    updates

--- DeliveryOptions.js
+++ DeliveryOptions.js
@@ -1,28 +1,45 @@
 
 $(function(){
-  // 1) Check if delivery cost row shows $250.00
+  // 1) Check delivery cost
   var costText = $('#ctl00_PageBody_CartSummary2_DeliveryCostsRow td.numeric').text().trim();
   if (costText === '$250.00') {
     console.log('[DeliveryCheck] High delivery cost detected:', costText);
 
-    // 2) Detach the BackToCart button and rename it
-    var $backBtn = $('#ctl00_PageBody_BackToCartButton3').detach();
-    $backBtn.find('span').text('Change Address');
+    // 2) Grab & detach the original “Back” anchor
+    var $origBack = $('#ctl00_PageBody_BackToCartButton3').detach();
+    var backHref = $origBack.attr('href');
 
-    // 3) Hard‐wire to ShoppingCart.aspx
-    $backBtn
-      .removeAttr('onclick')
-      .attr('href', 'ShoppingCart.aspx');
+    // 3) Build a “Change Address” button that reuses its postback
+    var $changeAddr = $(`
+      <a id="changeAddressBtn" class="epi-button mr-2" href="${backHref}">
+        <span>Change Address</span>
+      </a>
+    `);
 
-    // 4) Clear out everything else in .mainContents
-    var $container = $('.mainContents');
-    $container.empty();
+    // 4) Build a new “Edit Cart” button
+    var $editCart = $(`
+      <a id="editCartBtn" class="epi-button" href="ShoppingCart.aspx">
+        <span>Edit Cart</span>
+      </a>
+    `);
 
-    // 5) Re‐append just the Change Address button, centered
-    var $wrapper = $('<div class="text-center my-4"></div>').append($backBtn);
-    $container.append($wrapper);
+    // 5) Empty the mainContents and show a message + our two buttons
+    var $container = $('.mainContents').empty();
+    var $message = $(`
+      <div class="delivery-error-message mb-4">
+        An item in your cart is not eligible for delivery to your selected delivery address.
+        Please select a new address or remove the item from your cart.
+      </div>
+    `);
 
-    console.log('[DeliveryCheck] Switched to Change Address mode');
+    $container
+      .append($message)
+      .append($('<div class="button-group"></div>')
+        .append($changeAddr)
+        .append($editCart)
+      );
+
+    console.log('[DeliveryCheck] Switched to delivery-error view');
   }
 });
 
